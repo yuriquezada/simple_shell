7f93560b6290933fb61e714bcc660a185a5057f5
yuriquezada <quezadat.yuri@gmail.com>
2021-08-14 19:08:10 -0700

    Create test file tokenizer.c

---
+++ tokenizer.c
@@ -0,0 +1,29 @@
+#include "file.h"
+
+char **tokenizer(char *buffer, char *delimiter)
+{
+        char **tokens = NULL;
+        size_t i = 0;
+        unsigned int mcount = 10;
+
+        if (buffer == NULL)
+                return (NULL);
+        tokens = malloc(sizeof(char *) * mcount);
+        if (tokens == NULL)
+        {
+                return (NULL);
+        }
+        while ((tokens[i] = strtok(buffer, delimiter)) != NULL)
+        {
+                i++;
+                if (i == mcount)
+                {
+                        tokens = realloc(tokens, mcount);
+                        if (tokens == NULL)
+                        {
+                                return (NULL);
+                        }
+                }
+        }
+}
+
