e41cd575b9ee4738753d3b06ec4a56b80f69c6e6
yuriquezada <quezadat.yuri@gmail.com>
2021-08-16 02:38:57 -0700

    Replace spaces with tabs

--- tokenizer.c
+++ tokenizer.c
@@ -2,28 +2,27 @@
 
 char **tokenizer(char *buffer, char *delimiter)
 {
-        char **tokens = NULL;
-        size_t i = 0;
-        unsigned int mcount = 10;
+	char **tokens = NULL;
+	size_t i = 0;
+	unsigned int mcount = 10;
 
-        if (buffer == NULL)
-                return (NULL);
-        tokens = malloc(sizeof(char *) * mcount);
-        if (tokens == NULL)
-        {
-                return (NULL);
+	if (buffer == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * mcount);
+	if (tokens == NULL)
+	{
+		return (NULL);
         }
-        while ((tokens[i] = strtok(buffer, delimiter)) != NULL)
-        {
-                i++;
-                if (i == mcount)
+	while ((tokens[i] = strtok(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i == mcount)
                 {
-                        tokens = realloc(tokens, mcount);
-                        if (tokens == NULL)
+			tokens = realloc(tokens, mcount);
+			if (tokens == NULL)
                         {
-                                return (NULL);
-                        }
-                }
-        }
+				return (NULL);
+			}
+		}
+	}
 }
-
